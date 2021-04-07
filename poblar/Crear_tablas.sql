68cbbee5b689682a4518fa991d90e4ffbe9d9a53
JaviF98 <47720474+JaviF98@users.noreply.github.com>
2021-04-07 15:33:21 +0200

    carpeta con programas para poblar

---
+++ poblar/Crear_tablas.sql
@@ -0,0 +1,80 @@
+CREATE TABLE Genero
+(
+	ClvGenero			NUMBER,
+	Genero1			CHAR(64),
+	Genero2			CHAR(64)		NOT NULL,
+	Genero3			CHAR(64)		NOT NULL,
+	PRIMARY KEY(ClvGenero));
+
+
+CREATE TABLE MiembroCrew
+(
+	ClvMiembroCrew			NUMBER,
+	EsGuionista			NUMBER(1),
+	EsDirector			NUMBER(1),
+	Nombre			CHAR(64),
+	AnyoNacimiento			NUMBER		NOT NULL,
+	AnyoFallecimiento			NUMBER		NOT NULL,
+	PRIMARY KEY(ClvMiembroCrew));
+
+
+CREATE TABLE MiembroCast
+(
+	ClvMiembroCast			NUMBER,
+	NombrePersonaje			CHAR(64),
+	Nombre			CHAR(64)		NOT NULL,
+	AnyoNacimiento			NUMBER		NOT NULL,
+	AnyoFallecimiento			NUMBER		NOT NULL,
+	PRIMARY KEY(ClvMiembroCast));
+
+
+CREATE TABLE Pelicula
+(
+	ClvPelicula			NUMBER,
+	Titulo			CHAR(64)		NOT NULL,
+	EsAdulta			NUMBER(1),
+	Duracion			NUMBER,
+	AnyoEstreno			NUMBER,
+	PRIMARY KEY(ClvPelicula));
+
+
+CREATE TABLE Fecha
+(
+	ClvFecha			NUMBER,
+	Anyo			NUMBER,
+	Mes			NUMBER,
+	Dia			NUMBER,
+	DiaMesAnyo			DATE,
+	PRIMARY KEY(ClvFecha));
+
+
+CREATE TABLE Crew
+(
+	ClvCrew			NUMBER,
+	ClvMiembroCrew			NUMBER,
+	PRIMARY KEY(ClvCrew),
+	FOREIGN KEY(ClvMiembroCrew) REFERENCES MiembroCrew (ClvMiembroCrew));
+
+CREATE TABLE Cast
+(
+	ClvCast			NUMBER,
+	ClvMiembroCast			NUMBER,
+	PRIMARY KEY(ClvCast),
+	FOREIGN KEY(ClvMiembroCast) REFERENCES MiembroCast (ClvMiembroCast));
+
+CREATE TABLE Voto
+(
+	ClvVoto			NUMBER,
+	ClvCrew			NUMBER		NOT NULL,
+	ClvCast			NUMBER		NOT NULL,
+	ClvPelicula			NUMBER		NOT NULL,
+	ClvGenero			NUMBER		NOT NULL,
+	ClvFecha			NUMBER		NOT NULL,
+	Puntuacion			FLOAT,
+	PRIMARY KEY(ClvVoto),
+	FOREIGN KEY(ClvCrew) REFERENCES Crew (ClvCrew),
+	FOREIGN KEY(ClvCast) REFERENCES Cast (ClvCast),
+	FOREIGN KEY(ClvPelicula) REFERENCES Pelicula (ClvPelicula),
+	FOREIGN KEY(ClvGenero) REFERENCES Genero (ClvGenero),
+	FOREIGN KEY(ClvFecha) REFERENCES Fecha (ClvFecha));
+
